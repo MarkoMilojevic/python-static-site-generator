b15e3c58d9bfcc9fbac4df76b7e2458e8b027e52
Marko Milojevic <milojevicmarko92@gmail.com>
2021-11-21 18:42:07 +0100

    5.10 Calling error @staticmethod.

--- ssg/site.py
+++ ssg/site.py
@@ -35,7 +35,9 @@ class Site:
         if parser is not None:
             parser.parse(path, self.source, self.dest)
         else:
-            print("Not implemented.")
+            self.error(
+                "No parser for the {} extension, file skipped!".format(path.suffix)
+            )
 
     @staticmethod
     def error(message):
