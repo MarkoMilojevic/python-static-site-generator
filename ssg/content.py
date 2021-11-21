989f51ac97ec9d95064c1bd391278d3b8dc7ef3b
Marko Milojevic <milojevicmarko92@gmail.com>
2021-11-21 17:28:30 +0100

    4.6. Type property.

--- ssg/content.py
+++ ssg/content.py
@@ -22,3 +22,7 @@ class Content(Mapping):
     @property
     def body(self):
         return self.data["content"]
+
+    @property
+    def type(self):
+        return self.data["type"] if "type" in self.data else None
