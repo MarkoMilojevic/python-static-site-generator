8e4b28ccd90b2c231e3592ccad3e550859b6386d
Marko Milojevic <milojevicmarko92@gmail.com>
2021-11-21 16:11:53 +0100

    4.4. Content constructor.

--- ssg/content.py
+++ ssg/content.py
@@ -14,3 +14,7 @@ class Content(Mapping):
         metadata = load(fm, Loader=FullLoader)
 
         return cls(metadata, content)
+
+    def __init__(self, metadata, content) -> None:
+        self.data = metadata
+        self.data["content"] = content
